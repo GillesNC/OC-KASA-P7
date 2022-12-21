a3cc132c186ac25df90336dc3dad1dce63b2b167
GillesNC <linggilles@gmail.com>
2022-12-21 17:58:46 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import './styles/App.scss';
 import Home from './pages/home/Home';
-// import About from './pages/about/About';
-// import Flat from './pages/card/FlatCard';
-// import Error404 from './pages/error404/404';
+import About from './pages/about/About';
+import Flat from './pages/card/FlatCard';
+import Error404 from './pages/error404/404';
 import Header from './components/Header';
 // import Footer from './components/Footer';
 import { Route, Routes } from 'react-router-dom';
@@ -14,9 +14,9 @@ function App() {
       <Header />
         <Routes>
           <Route path='/'element={<Home />} />
-          {/* <Route path='/about'><About /></Route>
-          <Route path='/flat'><Flat /></Route>
-          <Route path='/flat'><Flat /></Route> */}
+          <Route path='/about' element={<About/>} />
+          <Route path='/flat' element={<Flat/>} />
+          <Route path='/*' element={<Error404/>} />
         </Routes>
       {/* <Footer /> */}
     </div>
