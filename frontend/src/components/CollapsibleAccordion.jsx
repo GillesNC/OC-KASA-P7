e6d2970661a237742f113acc2008c6f43176e0a2
GillesNC <linggilles@gmail.com>
2022-12-28 21:30:48 +0100

    Finish Page About

--- frontend/src/components/CollapsibleAccordion.jsx
+++ frontend/src/components/CollapsibleAccordion.jsx
@@ -1,26 +1,24 @@
 import React from "react";
 import { useState } from "react";
-import DataAbout from "../data/dataAbout.json";
 
-function CollapsibleAccordion () {
+function CollapsibleAccordion ({title, content}) {
     const [active, setActive] = useState(false);
     const handleClick = () => {
         setActive(!active);
     }
 
     return (
-        DataAbout.map((DataAbout) => (
-            <article className="accordion" key={DataAbout.title}>
-                <div className="accordion__title" onClick={handleClick}>
-                    <h2>{DataAbout.title}</h2>
-                    <span className="material-symbols-outlined">expand_less</span>
-                </div>
-                {active && <div className="accordion__content">
-                    <p>{DataAbout.content}</p>
-                </div>}
-            </article>
-        ))
-    );
+        <article className="accordion">
+            <div className="accordion__title" onClick={handleClick}>
+                <h2>{title}</h2>
+                {active ? <span className="material-symbols-outlined">expand_more</span> :
+                <span className="material-symbols-outlined">expand_less</span>}
+            </div>
+            {active && <div className="accordion__content">
+                <p>{content}</p>
+            </div>}
+        </article>
+    )    
 }
 
 export default CollapsibleAccordion;
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import BannerAbout from "../components/BannerAbout";
+import DataAbout from "../data/dataAbout.json";
 import CollapsibleAccordion from "../components/CollapsibleAccordion";
 
 function About () {
     return (
         <main>
             <BannerAbout />
-            <section className="about">
-                <div className="about__collapse">
-                    <CollapsibleAccordion />
-                </div>
+            <section className="about" >
+                {DataAbout.map (({title, content}) => (
+                    <CollapsibleAccordion title={title} content={content}/>
+                ))}
             </section>
         </main>
     )
