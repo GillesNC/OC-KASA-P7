e7670382b8983c689a991ea9d55d5d1c0bf8f14a
GillesNC <linggilles@gmail.com>
2023-01-11 21:23:28 +0100

    Fix pagination carrousel

--- frontend/src/components/Carrousel.jsx
+++ frontend/src/components/Carrousel.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import DataFlat from "../data/dataFlat.json";
 
@@ -10,6 +10,43 @@ function Carrousel () {
   const images = flatExist.pictures;
 
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [activeAutoplay, setActiveAutoplay] = useState(true);
+  const autoplayRef = useRef();
+
+  const settings = {
+    maxItems : images.length,
+    speed: 1500,
+    autoplay: true,
+    autoplaySpeed: 3000
+  }
+
+  const playTimer = () => {
+    console.log("play Timer");
+    setActiveAutoplay(true);
+  };
+
+  const pauseTimer = () => {
+    console.log("pause Timer");
+    setActiveAutoplay(false);
+  };
+
+  useEffect(() => {
+    if (settings.autoplay && activeAutoplay) {
+      clearTimeout(autoplayRef.current);
+      autoplayRef.current = setTimeout(() => {
+        next();
+      }, settings.autoplaySpeed);
+    }
+  });
+
+  const pagination = index => {
+    return (
+      <div className="pagination">
+        <div>{index + 1}</div>/{settings.maxItems}
+      </div>
+    );
+  };
+
   const prev = () => {
     const newIndex = currentIndex - 1;
     setCurrentIndex(newIndex < 0 ? images.length - 1 : newIndex);
@@ -20,16 +57,19 @@ function Carrousel () {
     setCurrentIndex(newIndex >= images.length ? 0 : newIndex);
   };
 
-  // const timer = setInterval(next, 2000)
-
   return (  
-    <div>
+    <article>
       <img src={images[currentIndex]} alt="Carousel" />
-      <div className="icon-arrow">
-        <span className="material-symbols-outlined prev" onClick={prev}>chevron_left</span>
-        <span className="material-symbols-outlined next" onClick={next}>chevron_right</span>
+      <div className="carrousel-container">
+        <div className="icon-arrow">
+          <span className="material-symbols-outlined prev" onClick={prev} onMouseEnter={pauseTimer} onMouseLeave={playTimer}>chevron_left</span>
+          <span className="material-symbols-outlined next" onClick={next} onMouseEnter={pauseTimer} onMouseLeave={playTimer}>chevron_right</span>
+        </div>
+        <div className="pagination-container">
+          {pagination(currentIndex)}
+        </div>
       </div>
-    </div>
+    </article>
   )
 }
 
